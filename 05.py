b5951e8d3795932bcfc88215d6788a76fbaaf2ec
LMesaric <luka.mesaric@fer.hr>
2020-12-05 11:27:09 +0100

    Add solution for day 5

---
+++ 05.py
@@ -0,0 +1,16 @@
+with open("inputs/5.txt") as f:
+    data = f.readlines()
+
+seat_ids = set()
+for line in data:
+    binary_str = line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+    seat_ids.add(int(binary_str, base=2))
+
+min_ = min(seat_ids)
+max_ = max(seat_ids)
+print("Max:", max_)
+
+for i in range(min_, max_ + 1):
+    if i not in seat_ids:
+        print("Mine:", i)
+        break
